933c38de93850b05f0bf4338d03c15ac9af0c0da
Noorulain25 <nuaahmed@gmail.com>
2021-09-28 12:32:29 +0100

    adding identity file

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SchoolsFriend.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchoolsFriend.Startup))]
+namespace SchoolsFriend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
